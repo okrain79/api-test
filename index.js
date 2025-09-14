3c118d36e6d19e308fca999996f1a80b44ad7226
okrain79 <nguyenkieuoanh1812006@gmail.com>
2025-09-15 01:15:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8080;  // đổi fallback sang 8080 cho khớp
 
 app.get("/", (req, res) => {
   res.send("Hello, Railway! 🚀");
